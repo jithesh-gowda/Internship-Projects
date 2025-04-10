5cfdf4e78649d18335f98bd60fc3eb4b0192b4f9
Jithesh Gowda T C <jitheshgowda02@gmail.com>
2025-04-10 22:17:15 +0530

    Create ecommerce.sql

---
+++ ecommerce/ecommerce.sql
@@ -0,0 +1,14 @@
+create database ecommerce;
+use ecommerce;
+CREATE TABLE dataset (
+    event_time TEXT,
+    event_type TEXT,
+    product_id INTEGER,
+    category_id REAL,
+    category_code TEXT,
+    brand TEXT,
+    price REAL,
+    user_id INTEGER,
+    user_session TEXT
+);
+![Image](https://github.com/user-attachments/assets/16b66724-b528-4dbd-b8c0-0c4f7f212e5d)
